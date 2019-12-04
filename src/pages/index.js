cbc3c2155000cfaa728b0b5ba37e888512c43af2
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-04 09:49:10 +0900

    render all markdown posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,42 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>
+      <h1>MyBlog</h1>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <span>
+            {node.frontmatter.title}{" "}
+            — {node.frontmatter.date}
+          </span>
+          <p>{node.excerpt}</p>
+          <a href={node.frontmatter.path}>read more...</a> 
+        </div>
+      ))}
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+            path
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
